c2b99a75ec064d98a07bf83ca513f4699540f951
Lakshana PM <pmlakshana123@gmail.com>
2025-09-06 20:51:20 +0530

    Time: 0 ms (100%), Space: 8.3 MB (62.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.c
+++ 0100-same-tree/0100-same-tree.c
@@ -6,14 +6,12 @@
  *     struct TreeNode *right;
  *
 **/
-bool sameOrNot(struct TreeNode* p, struct TreeNode* q){
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
     if(p==NULL&&q==NULL) return true;
-     if(p==NULL||q==NULL) return false;  
+    if(p==NULL||q==NULL) return false;  
     if(p->val!=q->val){
         return false;
     }
-    return sameOrNot(p->left,q->left)&&sameOrNot(p->right,q->right);
-}
-bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    return sameOrNot(p,q);
+    return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
 }
