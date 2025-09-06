421ee9dd302ed059f410d479998d7c84bffdf3f9
Lakshana PM <pmlakshana123@gmail.com>
2025-09-06 20:37:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (15.51%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if(!root) return root;
+    struct TreeNode* temp=root->left;
+    root->left=root->right;
+    root->right=temp;
+    if(root->left){
+    invertTree(root->left);}
+    if(root->right){
+        invertTree(root->right);}
+    return root;
+
+}
