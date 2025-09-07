9dc6d1b2ae8a0b10637dcb58249d17e73740356c
Lakshana PM <pmlakshana123@gmail.com>
2025-09-07 18:12:10 +0530

    Time: 0 ms (100%), Space: 9.6 MB (38.15%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.c
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.c
@@ -6,15 +6,15 @@
  *     struct TreeNode *right;
  * };
  */
-int leafsum(struct TreeNode* root,int* sum,int isleft,int isroot){
+int leafsum(struct TreeNode* root,int* sum,int isleft){
     if(root==NULL) return *sum;
-    if(root->left==NULL&&root->right==NULL&&isleft&&isroot!=1) *sum+=root->val;
-    leafsum(root->left,sum,1,0);
-    leafsum(root->right,sum,0,0);
+    if(root->left==NULL&&root->right==NULL&&isleft) *sum+=root->val;
+    leafsum(root->left,sum,1);
+    leafsum(root->right,sum,0);
     return *sum;
 }
 int sumOfLeftLeaves(struct TreeNode* root) {
     int sum=0;
-    leafsum(root,&sum,1,1);
+    leafsum(root,&sum,0);
     return sum;
 }
