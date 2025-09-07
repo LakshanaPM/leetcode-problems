7ab4cbf80bc7b5a2788141a91938db4e41964f71
Lakshana PM <pmlakshana123@gmail.com>
2025-09-07 19:05:00 +0530

    Time: 0 ms (100%), Space: 45.8 MB (21.91%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.c
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.c
@@ -6,14 +6,12 @@
  *     struct TreeNode *right;
  * };
  */
-void inorder(struct TreeNode* root, int low, int high,int* sum){
-    if(!root) return;
-    if(root->val>=low&&root->val<=high) *sum+=root->val;
-    inorder(root->left,low,high,sum);
-    inorder(root->right,low,high,sum);
+int inorder(struct TreeNode* root, int low, int high){
+    if(!root) return 0;
+    if(root->val<low) return inorder(root->right,low,high);
+    if(root->val>high) return inorder(root->left,low,high);
+    return root->val+inorder(root->left,low,high)+inorder(root->right,low,high);
 }
 int rangeSumBST(struct TreeNode* root, int low, int high) {
-    int sum=0;
-    inorder(root,low,high,&sum);
-    return sum;
+    return inorder(root,low,high);
 }
