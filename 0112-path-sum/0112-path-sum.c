6e2265bb64691ea9a144d86e8e0a53ef8c40501b
Lakshana PM <pmlakshana123@gmail.com>
2025-10-07 20:16:15 +0530

    Time: 0 ms (100%), Space: 11.5 MB (67.5%) - LeetHub

--- 0112-path-sum/0112-path-sum.c
+++ 0112-path-sum/0112-path-sum.c
@@ -6,15 +6,8 @@
  *     struct TreeNode *right;
  * };
  */
-
-bool preorder(struct TreeNode* root,int targetSum,int sum){
-    if(root==NULL) return false;
-    sum+=root->val;
-    if (root->left == NULL && root->right == NULL && sum == targetSum)
-        return true;
-    
-    return preorder(root->left,targetSum,sum)||preorder(root->right,targetSum,sum);
-}
 bool hasPathSum(struct TreeNode* root, int targetSum) {
-    return preorder(root,targetSum,0);
+    if(!root) return false;
+    if(targetSum==root->val&&!root->left&&!root->right) return true;
+    return hasPathSum(root->left, targetSum-root->val)||hasPathSum(root->right, targetSum-root->val);
 }
