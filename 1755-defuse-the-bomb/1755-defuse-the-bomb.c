5f40bbf34731770e1a9c97b5dc99d0dc7e8a1ecb
Lakshana PM <pmlakshana123@gmail.com>
2025-10-19 12:30:10 +0530

    Time: 0 ms (100%), Space: 10.1 MB (95.74%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.c
@@ -0,0 +1,43 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* decrypt(int* code, int codeSize, int k, int* returnSize) {
+    *returnSize=codeSize;
+    if(k==0){
+        for(int i=0;i<codeSize;i++){
+            code[i]=0;
+        }
+        return code;
+    }
+    int* result=(int*)malloc(codeSize*sizeof(int));
+    int i=0;
+    if(k>0){
+        for(i=0;i<codeSize;i++){
+            int sum=0,x=0,y=i+1;
+            while(x<k){
+                if(y==codeSize){
+                    y=0;
+                }
+                sum=sum+code[y];
+                y++;
+                x++;
+            }
+            result[i]=sum;
+        }
+    }
+    else if(k<0){
+        for(i=codeSize-1;i>=0;i--){
+            int sum=0,x=0,y=i-1;
+            while(x>k){
+                if(y==-1){
+                    y=codeSize-1;
+                }
+                sum=sum+code[y];
+                y--;
+                x--;
+            }
+            result[i]=sum;
+        }
+    }
+    return result;
+}
