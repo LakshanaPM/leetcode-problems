cb3c20a53864b3a9ce40a9be7cdfe8d961a2a673
Lakshana PM <pmlakshana123@gmail.com>
2025-10-23 19:14:16 +0530

    Time: 2 ms (37.68%), Space: 10.5 MB (36.23%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.c
+++ 0695-max-area-of-island/0695-max-area-of-island.c
@@ -1,13 +1,8 @@
 int areaCount(int** grid,int gridSize,int* gridColSize,int r,int c){
-    
     if(r<0||c<0||r>=gridSize||c>=gridColSize[r]||grid[r][c]==0) return 0;
     grid[r][c] = 0;
-    int area=1;
-    area+=areaCount(grid,gridSize,gridColSize,r+1,c);
-    area+=areaCount(grid,gridSize,gridColSize,r-1,c);
-    area+=areaCount(grid,gridSize,gridColSize,r,c+1);
-    area+=areaCount(grid,gridSize,gridColSize,r,c-1);
-    return area;
+    return areaCount(grid,gridSize,gridColSize,r+1,c)+areaCount(grid,gridSize,gridColSize,r-1,c)+areaCount(grid,gridSize,gridColSize,r,c+1)+areaCount(grid,gridSize,gridColSize,r,c-1)+1;
+    
 }
 int maxAreaOfIsland(int** grid, int gridSize, int* gridColSize) {
     int maxCount=0;
