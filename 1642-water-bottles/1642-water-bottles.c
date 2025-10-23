300f0744020972af7f48d936d997c482e7714569
Lakshana PM <pmlakshana123@gmail.com>
2025-10-23 14:08:31 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.56%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.c
+++ 1642-water-bottles/1642-water-bottles.c
@@ -1,10 +1,3 @@
 int numWaterBottles(int numBottles, int numExchange) {
-    int sum=numBottles,exchange=sum,remainder=0;
-    while((exchange+remainder)>=numExchange){
-        exchange+=remainder;
-        remainder=exchange%numExchange;
-        exchange=exchange/numExchange;
-        sum+=exchange;
-    }
-    return sum;
+    return numBottles+(numBottles-1)/(numExchange-1);
 }
