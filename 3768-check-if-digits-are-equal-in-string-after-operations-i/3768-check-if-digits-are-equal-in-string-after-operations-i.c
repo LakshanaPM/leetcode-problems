8c3b9c707d39031f9abcee2b8b80f05dbdfda320
Lakshana PM <pmlakshana123@gmail.com>
2025-10-23 08:07:57 +0530

    Time: 6 ms (0%), Space: 8.9 MB (6.82%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.c
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.c
@@ -4,8 +4,7 @@ bool hasSameDigits(char* s) {
         int k=0;
         char* result=(char*)malloc((len)*sizeof(char));
         for(int i=0;i<len-1;i++){
-            int c=(s[i]-'0'+s[i+1]-'0')%10;
-            result[k++]=c+'0';
+            result[k++]=(s[i]-'0'+s[i+1]-'0')%10+'0';
         }
         result[k]='\0';
         strcpy(s,result);
