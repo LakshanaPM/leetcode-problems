5b502113de3ad634c4743abb17dc8d7adabc39ee
Lakshana PM <pmlakshana123@gmail.com>
2025-10-25 12:48:52 +0530

    Time: 77 ms (6.73%), Space: 98.9 MB (55.16%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.c
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.c
@@ -1,29 +1,21 @@
 
 struct ListNode* mergeNodes(struct ListNode* head) {
     struct ListNode* node=head;
-    struct ListNode* previous=NULL;
-   
+    struct ListNode* previous=head;
     while(node&&node->next){
-        if(node->val==0){
-        struct ListNode* curr=node;
         int sum=0;
-        while(curr->next&&curr->next->val!=0){
-            sum+=curr->next->val;
-            struct ListNode* todelete=curr->next;    
-            curr->next=todelete->next;
+        while(node->next&&node->next->val!=0){
+            sum+=node->next->val;
+            struct ListNode* todelete=node->next;    
+            node->next=todelete->next;
             free(todelete);
         }
         node->val=sum;
         previous=node;
-        node=node->next;}
-        else{
-            node=node->next;
-        }
-        
+        node=node->next;
     }
     if(previous){
         previous->next=NULL;
     }
-
     return head;
 }
