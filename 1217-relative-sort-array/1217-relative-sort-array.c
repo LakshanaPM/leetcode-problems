2da698783eccb66c1d501a2084715781f157f9ea
Lakshana PM <pmlakshana123@gmail.com>
2025-11-08 20:06:01 +0530

    Time: 0 ms (100%), Space: 9.8 MB (59.04%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.c
+++ 1217-relative-sort-array/1217-relative-sort-array.c
@@ -7,22 +7,24 @@ int compare(const void *a, const void *b) {
 int* relativeSortArray(int* arr1, int arr1Size, int* arr2, int arr2Size, int* returnSize) {
     int* result=(int*)malloc((arr1Size)*sizeof(int));
     *returnSize=0;
-    int* check=(int*)calloc(arr1Size,sizeof(int));
+    int* count=(int*)calloc(1001,sizeof(int));
     qsort(arr1,arr1Size,sizeof(int),compare);
-    for(int i=0;i<arr2Size;i++){
-        for(int j=0;j<arr1Size;j++){
-            if(arr2[i]==arr1[j]){
-                result[(*returnSize)++]=arr2[i];
-                check[j]=1;
-            }
-        }
+    for(int i=0;i<arr1Size;i++){
+        count[arr1[i]]++;
     }
     
-    for(int i=0;i<arr1Size;i++){
-        if(check[i]==0){
-            result[(*returnSize)++]=arr1[i];
+    for(int i=0;i<arr2Size;i++){
+        while(count[arr2[i]]>0){
+            result[(*returnSize)++]=arr2[i];
+            count[arr2[i]]--;
         }
     }
-    free(check);
+    for(int i=0;i<1001;i++){
+        while(count[i]>0){
+            result[(*returnSize)++]=i;
+            count[i]--;
+        }
+    }
+    free(count);
     return result;
 }
