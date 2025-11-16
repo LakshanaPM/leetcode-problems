ef2f459a2242acb97fffc7b0729e32dc98a511da
Lakshana PM <pmlakshana123@gmail.com>
2025-11-16 20:06:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (45.83%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.c
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.c
@@ -1,17 +1,21 @@
-
 bool areAlmostEqual(char* s1, char* s2) {
     int i=0;
     int count=0;
-    int r[2];
-    while(s1[i]!='\0'){
+    int f=-1,s=-1;
+    while(s1[i]!='\0'&&s2[i]!='\0'){
         if(s1[i]!=s2[i]){
-        if(count>=2) return false;
-        r[count++]=i;}
+            count++;
+            if(count==1) f=i;
+            if(count==2) s=i;
+            else if(count>2){ 
+                return false;
+            }
+        }
         i++;
-    } 
-    char s=s1[r[0]];
-    s1[r[0]]=s1[r[1]];
-    s1[r[1]]=s;
-    if(count==2&&s1[r[0]]==s2[r[0]]&&s1[r[1]]==s2[r[1]]) return true;
+    }
+    if (s1[i] != s2[i]) return false;
+    if(count==2){
+        return s1[f]==s2[s]&&s1[s]==s2[f];
+    }
     return count==0;
 }
