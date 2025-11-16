90e97787e6dddfc6545337b2f423b979fe7b5af5
Lakshana PM <pmlakshana123@gmail.com>
2025-11-16 16:56:26 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.12%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.c
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.c
@@ -1,13 +1,9 @@
 int balancedStringSplit(char* s) {
-    int left=0,right=0,i=0,count=0;
+    int right=0,i=0,count=0;
     while(s[i]!='\0'){
         if(s[i]=='R') right++;
-        if(s[i]=='L') left++;
-        if(left==right&&left>0){
-            count++;
-            left=0;
-            right=0;
-        }
+        else right--;
+        if(right==0) count++;
         i++;
     }
     return count;
