a08e2b40b3eb0a0fa3b40a4c2866d49458f4e227
Lakshana PM <pmlakshana123@gmail.com>
2025-11-17 20:02:30 +0530

    Time: 0 ms (100%), Space: 9 MB (84.21%) - LeetHub

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.c
@@ -0,0 +1,17 @@
+bool makeEqual(char** words, int wordsSize) {
+    int* count=(int*)calloc(26,sizeof(int));
+    if(wordsSize==1) return true;
+    for(int i=0;i<wordsSize;i++){
+        int j=0;
+        while(words[i][j]!='\0'){
+            count[words[i][j]-'a']++;
+            j++;
+        }
+    }
+    for(int i=0;i<26;i++){
+        if(count[i]%wordsSize!=0){
+            return false;
+        }
+    }
+    return true;
+}
