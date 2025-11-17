790c978245d60ebb70c9b439251b31c98417acab
Lakshana PM <pmlakshana123@gmail.com>
2025-11-17 20:18:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.71%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.c
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.c
@@ -1,14 +1,14 @@
 bool checkAlmostEquivalent(char* word1, char* word2) {
     int* count1=(int*)calloc(26,sizeof(int));
-    int* count2=(int*)calloc(26,sizeof(int));
     int i=0;
     while(word1[i]!='\0'){
         count1[word1[i]-'a']++;
-        count2[word2[i]-'a']++;
+        count1[word2[i]-'a']--;
         i++;
     }
-    for(int j=0;j<26;j++){
-        if(abs(count1[j]-count2[j])>3) return false;
+    i=0;
+    for(i=0;i<26;i++){
+        if(count1[i]>3||count1[i]<-3) return false;
     }
     return true;
 }
