3571db14f5e910c5dd91e0dbc43fc61990c96b3a
Lakshana PM <pmlakshana123@gmail.com>
2025-11-17 19:50:25 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.71%) - LeetHub

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.c
@@ -0,0 +1,13 @@
+bool kLengthApart(int* nums, int numsSize, int k) {
+    int x=-1;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]==1&&x==-1) x=i;
+        else if(nums[i]==1){
+            if(i-x-1<k){
+                return false;
+            }
+            x=i;
+        }
+    }
+    return true;
+}
