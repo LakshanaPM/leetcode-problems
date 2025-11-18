f8d80119a4b97f6df9da3aaba04bddb94a785f5d
Lakshana PM <pmlakshana123@gmail.com>
2025-11-18 11:59:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (90%) - LeetHub

--- 2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.c
+++ 2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.c
@@ -1,9 +1,11 @@
+int isVowel(char s){
+    return s=='a'||s=='e'||s=='o'||s=='u'||s=='i';
+}
 int vowelStrings(char** s, int wordsSize, int left, int right) {
     int count=0;
     for(int i=left;i<=right;i++){
-        if(s[i][0]!='a'&&s[i][0]!='e'&&s[i][0]!='u'&&s[i][0]!='i'&&s[i][0]!='o') continue;
         int l=strlen(s[i])-1;
-        if(s[i][l]=='a'||s[i][l]=='e'||s[i][l]=='u'||s[i][l]=='i'||s[i][l]=='o'){
+        if(isVowel(s[i][0])&&isVowel(s[i][l])) {
             count++;
         }
     }
