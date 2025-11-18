3b4cb9df517446da1927ac46a81b8f5709879c32
Lakshana PM <pmlakshana123@gmail.com>
2025-11-18 15:26:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (36.11%) - LeetHub

--- 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.c
+++ 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.c
@@ -1,7 +1,7 @@
 int countTestedDevices(int* batteryPercentages, int batteryPercentagesSize) {
     int count=0;
     for(int i=0;i<batteryPercentagesSize;i++){
-        if((batteryPercentages[i]-count)>0){
+        if(batteryPercentages[i]>count){
             count++;
         }
     }
