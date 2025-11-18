cc0270061e793f0957f223f4c242ba92a6587f7f
Lakshana PM <pmlakshana123@gmail.com>
2025-11-18 07:47:02 +0530

    Time: 7 ms (80%), Space: 8.7 MB (40%) - LeetHub

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.c
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.c
@@ -1,8 +1,13 @@
 int numOfPairs(char** nums, int numsSize, char* target) {
     int count=0;
+    int* len=(int*)malloc(numsSize*sizeof(int));
+    int l=strlen(target);
+    for(int i=0;i<numsSize;i++){
+        len[i]=strlen(nums[i]);
+    }
     for(int i=0;i<numsSize;i++){
         for(int j=0;j<numsSize;j++){
-            if(i==j) continue;
+            if(i==j||len[i]+len[j]!=l) continue;
             char t[250];
             strcpy(t, nums[i]);
             if(strcmp(target,strcat(t,nums[j]))==0) count++;
