0caf345337c1383ef96be916ff4b735b0d855648
Lakshana PM <pmlakshana123@gmail.com>
2025-11-18 11:09:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80%) - LeetHub

--- 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.c
+++ 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.c
@@ -1,17 +1,19 @@
 int mostFrequent(int* nums, int numsSize, int key) {
+    int max=nums[0];
+    for(int i=1;i<numsSize;i++){
+        if(nums[i]>max){
+            max=nums[i];
+        }
+    }
+    int* count=(int*)calloc((max+1),sizeof(int));
     int result=-1,maxCount=0;
     for(int i=0;i<=numsSize-2;i++){
-        if(nums[i]==key){
-            int count=0,target=nums[i+1];
-            for(int j=0;j<numsSize;j++){
-                if(nums[j]==key&&nums[j+1]==target){
-                    count++;
-                }
-            }
-            if(count>maxCount){
-                result=target;
-                maxCount=count;
-            }
+        if(nums[i]==key) count[nums[i+1]]++;
+    }
+    for(int i=0;i<numsSize;i++){
+        if(count[nums[i]]>maxCount){
+            maxCount=count[nums[i]];
+            result=nums[i];
         }
     }
     return result;
