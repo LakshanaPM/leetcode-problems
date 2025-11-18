0c18e310c7f9f15cc57d114fca89e59e43d9e505
Lakshana PM <pmlakshana123@gmail.com>
2025-11-18 11:26:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (77.31%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.c
@@ -0,0 +1,8 @@
+int maximumCount(int* nums, int numsSize) {
+    int min=0,max=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]<0) min++;
+        else if(nums[i]>0) max++;
+    }
+    return max>min?max:min;
+}
