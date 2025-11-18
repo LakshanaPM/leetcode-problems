20688a170574974ead8bd4459417a54f896e88d6
Lakshana PM <pmlakshana123@gmail.com>
2025-11-18 11:40:36 +0530

    Time: 152 ms (25.45%), Space: 9.8 MB (47.27%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.c
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.c
@@ -1,21 +1,21 @@
 int mostFrequentEven(int* nums, int numsSize) {
     int maxCount=0,min=INT_MAX;
     for(int i=0;i<numsSize;i++){
-        if(nums[i]%2==0){
-            int count=0;
-            for(int j=0;j<numsSize;j++){
-                if(nums[i]==nums[j]) {
-                    count++;
-                }
-            }
-            if(count>maxCount){ 
-                maxCount=count;
-                min=nums[i];
-            }
-            else if(count==maxCount&&nums[i]<min){
-                min=nums[i];
+        if(nums[i]%2!=0) continue;
+        int count=0;
+        for(int j=0;j<numsSize;j++){
+            if(nums[i]==nums[j]) {
+                count++;
             }
         }
+        if(count>maxCount){ 
+            maxCount=count;
+            min=nums[i];
+        }
+        else if(count==maxCount&&nums[i]<min){
+            min=nums[i];
+       }
     }
+    
     return min==INT_MAX?-1:min;
 }
