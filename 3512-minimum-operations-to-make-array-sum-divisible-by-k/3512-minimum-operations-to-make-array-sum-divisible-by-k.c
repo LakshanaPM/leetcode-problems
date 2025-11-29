3f5c51ed18bb89a35bc186f61b3a1011e8987b95
Lakshana PM <pmlakshana123@gmail.com>
2025-11-29 07:56:17 +0530

    Time: 0 ms (100%), Space: 10.4 MB (65.57%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.c
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.c
@@ -1,15 +1,7 @@
 int minOperations(int* nums, int numsSize, int k) {
-    int sum=0,i=0;
-    for(i=0;i<numsSize;i++){
+    int sum=0;
+    for(int i=0;i<numsSize;i++){
         sum+=nums[i];
     }
-    if(sum%k==0){
-        return 0;
-    }
-    i=0;
-    while(sum%k!=0){
-        sum-=1;
-        i++;
-    }
-    return i;
+    return sum%k;
 }
