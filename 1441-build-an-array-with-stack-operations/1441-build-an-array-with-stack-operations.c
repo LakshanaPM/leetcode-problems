d16fdc78225b568f963c8d1a213c2b60908c0ece
Lakshana PM <pmlakshana123@gmail.com>
2025-11-30 09:02:11 +0530

    Time: 0 ms (100%), Space: 8.9 MB (97.76%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.c
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.c
@@ -2,27 +2,32 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char** buildArray(int* target, int targetSize, int n, int* returnSize) {
-    char* p1="Push";
-    char* p2="Pop";
-    char** result=(char**)malloc((n*n)*sizeof(char*));
-    int j=0,k=0;
+    int j=0,count=0;
     for(int i=1;i<=n&&j<targetSize;i++){
-        if(target[j]!=i){
-            result[k]=(char*)malloc(5);
-            strcpy(result[k],p1);
-            k++;
-            result[k]=(char*)malloc(4);
-            strcpy(result[k],p2);
-            k++;
+        if(target[j]==i){
+            count++;
+            j++;
         }
         else{
-            result[k]=(char*)malloc(5);
-            strcpy(result[k],p1);
-            k++;
+            count+=2;
+        }
+    }
+    *returnSize=count;
+    char** result=(char**)malloc(count*sizeof(char*));
+    j=0,count=0;
+    for(int i=1;i<=n&&j<targetSize;i++){
+        if(target[j]!=i){
+            result[count]="Push";
+            count++;
+            result[count]="Pop";
+            count++;
+        }
+        else{
+            result[count]="Push";
+            count++;
             j++;
         }
     }
-    *returnSize=k;
     return result;
     
 }
