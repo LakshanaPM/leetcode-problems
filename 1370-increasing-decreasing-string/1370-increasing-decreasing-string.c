31a2bebdb587483d8ea55edb8ef69d88656068d1
Lakshana PM <pmlakshana123@gmail.com>
2025-12-01 19:49:47 +0530

    Time: 4 ms (0%), Space: 9.1 MB (0%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.c
@@ -0,0 +1,27 @@
+char* sortString(char* s) {
+    int* count=(int*)calloc(26,sizeof(int));
+    int i=0;
+    while(s[i]!='\0'){
+        count[s[i]-'a']++;
+        i++;
+    }
+    int l=strlen(s);
+    int j=0;
+    char* result=(char*)malloc(l*sizeof(int));
+    while(j<l){
+        for(int i=0;i<26;i++){
+            if(count[i]>0){
+                result[j++]=i+'a';
+                count[i]--;
+            }
+        }
+        for(int i=26-1;i>=0;i--){
+            if(count[i]>0){
+                result[j++]=i+'a';
+                count[i]--;
+            }
+        }
+    }
+    result[l]='\0';
+    return result;
+}
