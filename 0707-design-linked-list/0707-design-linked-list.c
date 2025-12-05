6eb201e6574595c7065ddf25edd1752f7c44efa3
Lakshana PM <pmlakshana123@gmail.com>
2025-12-05 15:02:58 +0530

    Time: 20 ms (16.07%), Space: 19.6 MB (29.36%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.c
@@ -0,0 +1,92 @@
+
+
+
+typedef struct MyLinkedList{
+    int val;
+    struct MyLinkedList* next;
+} MyLinkedList;
+
+
+MyLinkedList* myLinkedListCreate() {
+    MyLinkedList* newnode=(MyLinkedList*)malloc(sizeof(MyLinkedList));
+    newnode->next=NULL;
+    return newnode;
+}
+
+int myLinkedListGet(MyLinkedList* obj, int index) {
+    int i=0;
+    MyLinkedList* curr=obj->next;
+    while(curr&&i<index){
+        curr=curr->next;
+        i++;
+    }
+    if(curr){
+        return curr->val;
+    }
+    return -1;
+}
+
+void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+    MyLinkedList* newnode=myLinkedListCreate();
+    newnode->val=val;
+    newnode->next=obj->next;
+    obj->next=newnode;
+}
+
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+    MyLinkedList* curr=obj;
+    while(curr->next){
+        curr=curr->next;
+    }
+    MyLinkedList* newnode=myLinkedListCreate();
+    newnode->val=val;
+    newnode->next=NULL;
+    curr->next=newnode;
+}
+
+void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+    int i=0;
+    MyLinkedList* curr=obj;
+    while(curr&&i<index){
+        curr=curr->next;
+        i++;
+    }
+    if(!curr) return;
+    MyLinkedList* newnode=myLinkedListCreate();
+    newnode->val=val;
+    newnode->next=curr->next;
+    curr->next=newnode;
+}
+
+void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+    int i=0;
+    MyLinkedList* curr=obj;
+    while(curr->next&&i<index){
+        curr=curr->next;
+        i++;
+    }
+    if(!curr->next) return;
+    MyLinkedList* newnode=curr->next;
+    curr->next=curr->next->next;
+    free(newnode);
+}
+
+void myLinkedListFree(MyLinkedList* obj) {
+    free(obj);
+}
+
+/**
+ * Your MyLinkedList struct will be instantiated and called as such:
+ * MyLinkedList* obj = myLinkedListCreate();
+ * int param_1 = myLinkedListGet(obj, index);
+ 
+ * myLinkedListAddAtHead(obj, val);
+ 
+ * myLinkedListAddAtTail(obj, val);
+ 
+ * myLinkedListAddAtIndex(obj, index, val);
+ 
+ * myLinkedListDeleteAtIndex(obj, index);
+ 
+ * myLinkedListFree(obj);
+*/
