725d89097c045fa7c01ff395a994a3ee15102582
Lakshana PM <pmlakshana123@gmail.com>
2025-12-05 17:27:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.c
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head,int ub){
+    struct ListNode* prev=NULL;
+    struct ListNode* curr=head;
+    struct ListNode* nextNode=curr;
+    int i=0;
+    if(!head) return head;
+    while(curr&&i<ub){
+        nextNode=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nextNode;
+        i++;
+    }
+    head->next=curr;
+    return prev;
+}
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+    struct ListNode* curr=head;
+    if(!head||!head->next||k==0) return head;
+    int i=0;
+    while(curr){
+        i++;
+        curr=curr->next;
+    }
+    if(i==k) return head;
+    k=k%i;
+    head=reverseList(head,i);
+    struct ListNode* first=head;
+    curr=reverseList(first,k);
+    struct ListNode* second=first->next;
+    if(second) first->next=reverseList(second,i-k);
+    return curr;
+}
