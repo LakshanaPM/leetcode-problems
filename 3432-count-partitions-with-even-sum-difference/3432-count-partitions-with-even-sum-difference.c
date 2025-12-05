2e42e9225bb15c71c8ff7c88514c49c807277b9c
Lakshana PM <pmlakshana123@gmail.com>
2025-12-05 10:49:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (42.11%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.c
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.c
@@ -3,14 +3,6 @@ int countPartitions(int* nums, int numsSize) {
     for(int i=0;i<numsSize;i++){
         sum+=nums[i];
     }
-    int sum1=0;
-    int count=0;
-    for(int i=0;i<numsSize-1;i++){
-        sum1=sum1+nums[i];
-        sum=sum-nums[i];
-        if((sum1%2==0&&sum%2==0)||(sum1%2!=0&&sum%2!=0)){
-            count++;
-        }
-    }
-    return count;
+    
+    return sum%2==0?numsSize-1:0;
 }
