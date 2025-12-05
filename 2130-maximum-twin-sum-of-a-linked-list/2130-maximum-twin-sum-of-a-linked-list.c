45ef2f630bd417d496618e8f4f3698676bc6cb76
Lakshana PM <pmlakshana123@gmail.com>
2025-12-05 11:36:32 +0530

    Time: 0 ms (100%), Space: 50.4 MB (52.02%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+int pairSum(struct ListNode* head) {
+    struct ListNode* slow=head;
+    struct ListNode* fast=head;
+    while(fast&&fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    struct ListNode* prev=NULL;
+    struct ListNode* curr=slow;
+    struct ListNode* nxt=curr;
+    while(curr){
+        nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nxt;
+    }
+    int max=INT_MIN;
+    curr=head;
+    slow=prev;
+    while(slow){
+        int sum=curr->val+slow->val;
+        if(sum>max){
+            max=sum;
+        }
+        curr=curr->next;
+        slow=slow->next;
+    }
+    return max;
+}
