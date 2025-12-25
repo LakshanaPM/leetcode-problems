d74002f120807b2602c9bfe67c6349e5fa9ca88e
Lakshana PM <pmlakshana123@gmail.com>
2025-12-25 16:03:26 +0530

    Time: 1 ms (93.44%), Space: 43.2 MB (37.78%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -4,18 +4,17 @@ class Solution {
         int n2=word2.length();
         int i=0;
         int j=0;
-        String s="";
+        StringBuilder sb = new StringBuilder();
         while(i<n1||j<n2){
             if(i<n1){
-                s+=word1.charAt(i);
+                sb.append(word1.charAt(i));
                 i++;
             }
             if(j<n2){
-                s+=word2.charAt(j);
+                sb.append(word2.charAt(j));
                 j++;
             }
         }
-        return s;
-
+        return sb.toString();
     }
 }
