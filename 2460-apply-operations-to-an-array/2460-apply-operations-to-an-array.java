46647973cc7c666c69bd5f30c1ae81d17b9f80a6
Lakshana PM <pmlakshana123@gmail.com>
2025-12-26 11:56:43 +0530

    Time: 1 ms (94.83%), Space: 45.1 MB (68.7%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -11,11 +11,12 @@ class Solution {
         int j=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[j++]=nums[i];
+                if(i!=j){
+                    nums[j]=nums[i];
+                    nums[i]=0;
+                }
+                j++;
             }
-        }
-        for(int i=j;i<n;i++){
-            nums[i]=0;
         }
         return nums;
     }
