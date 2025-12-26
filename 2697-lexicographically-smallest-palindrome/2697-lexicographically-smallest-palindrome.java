9e86c2b2ded9e6d5554e3a9677e74f97765c85d6
Lakshana PM <pmlakshana123@gmail.com>
2025-12-26 12:33:22 +0530

    Time: 12 ms (19.62%), Space: 46.8 MB (83.45%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        int i=0;
+        int j=sb.length()-1;
+        while(i<j){
+            if(sb.charAt(i)<sb.charAt(j)){
+                sb.setCharAt(j,sb.charAt(i));
+            }
+            else if(sb.charAt(i)>sb.charAt(j)){
+                sb.setCharAt(i,sb.charAt(j));
+            }
+            i++;
+            j--;
+        }
+        return sb.toString();
+    }
+}
