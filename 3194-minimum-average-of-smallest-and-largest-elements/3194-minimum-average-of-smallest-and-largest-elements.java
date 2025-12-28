049fa5ec7c3ce4414f8fa7b67dcec71d5836918e
Lakshana PM <pmlakshana123@gmail.com>
2025-12-28 15:42:17 +0530

    Time: 3 ms (93.4%), Space: 48.6 MB (5.53%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.java
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.java
@@ -11,13 +11,11 @@ class Solution {
         int start = 0;
         int end = n-1;
         double min=9999.0;
-        while(start<end){
-            double average=(nums[start]+nums[end])/2.0;
+        for(int i = 0; i < n/2; i++) {
+            double average = (nums[i] + nums[n-1-i])/2.0;
             if(average<min){
                 min=average;
             }
-            start++;
-            end--;
         }
         return min;
     }
