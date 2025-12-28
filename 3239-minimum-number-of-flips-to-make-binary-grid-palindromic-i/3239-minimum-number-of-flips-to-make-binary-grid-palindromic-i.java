87b1bcfaeb3107b7571bb68a7e8424e46134e3b1
Lakshana PM <pmlakshana123@gmail.com>
2025-12-28 17:52:18 +0530

    Time: 6 ms (97.17%), Space: 186.9 MB (33.02%) - LeetHub

---
+++ 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minFlips(int[][] grid) {
+        int count=0;
+        int n=grid[0].length;
+        for(int i=0;i<grid.length;i++){
+            int l=0,r=n-1;
+            while(l<r){
+                if(grid[i][l]!=grid[i][r]){
+                    count++;
+                }
+                l++;
+                r--;
+            }
+        }
+        int count1=0;
+        for(int i=0;i<n;i++){
+            int l=0,r=grid.length-1;
+            while(l<r){
+                if(grid[l][i]!=grid[r][i]){
+                    count1++;
+                }
+                l++;
+                r--;
+            }
+        }
+        if(count1<count) return count1;
+        return count;
+    }
+}
