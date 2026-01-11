a5c4366a655caa21322915d6ea09f4aeb2b26dab
Lakshana PM <pmlakshana123@gmail.com>
2026-01-11 10:14:24 +0530

    Time: 0 ms (100%), Space: 46.2 MB (83.85%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -5,14 +5,9 @@ class Solution {
         if(arr.length==1){
             return arr[0];
         }
-        for(int i=1;i<arr.length;i++){
-            if (arr[i] == arr[i - 1]) {
-                count++;
-                if (count > threshold) {
-                    return arr[i];
-                }
-            } else {
-                count = 1; 
+        for(int i=0;i<arr.length-threshold;i++){
+            if (arr[i] == arr[i+threshold]) {
+                return arr[i];
             }
         }
         return -1;
