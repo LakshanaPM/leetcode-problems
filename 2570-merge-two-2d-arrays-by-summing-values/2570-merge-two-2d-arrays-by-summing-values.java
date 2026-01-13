c01597fa40fde6f8f3e419433b29eaeb9a4895da
Lakshana PM <pmlakshana123@gmail.com>
2026-01-13 11:27:21 +0530

    Time: 1 ms (88.16%), Space: 47 MB (31.29%) - LeetHub

---
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int n1=nums1.length,n2=nums2.length;        
+        int[][] merge=new int[n1+n2][2];
+        int i=0,j=0,k=0;
+        while(i<n1&&j<n2){
+            if(nums1[i][0]<nums2[j][0]){
+                merge[k][0]=nums1[i][0];
+                merge[k][1]=nums1[i][1];
+                i++;
+            }
+            else if(nums1[i][0]>nums2[j][0]){
+                merge[k][0]=nums2[j][0];
+                merge[k][1]=nums2[j][1];
+                j++;
+            }
+            else{
+                merge[k][0]=nums2[j][0];
+                merge[k][1]=nums2[j][1]+nums1[i][1];
+                j++;
+                i++;
+            }
+            k++;
+        }
+        while (i < n1) merge[k++] = nums1[i++];
+        while (j < n2) merge[k++] = nums2[j++];
+        int[][] result = new int[k][2];
+        for (int x = 0; x < k; x++) {
+            result[x] = merge[x];
+        }
+        return result;
+    }
+}
