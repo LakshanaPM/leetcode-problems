9c1e505442e3aed8a097c5fd599f64cdc14e8496
Lakshana PM <pmlakshana123@gmail.com>
2026-01-25 14:01:57 +0530

    Time: 0 ms (100%), Space: 17.4 MB (60.47%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool match(struct ListNode* head, struct TreeNode* root){
+    if(head==NULL) return true;
+    if(root==NULL) return false;
+    if(head->val!=root->val) return false;
+    return match(head->next,root->left)||match(head->next,root->right);
+}
+bool isSubPath(struct ListNode* head, struct TreeNode* root) {
+    if(root==NULL) return false;
+    if(match(head,root)) return true;
+    return isSubPath(head,root->left)||isSubPath(head,root->right);
+}
