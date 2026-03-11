9be9fcd40384c2ec8c750708d8265118af155b53
Lakshana PM <pmlakshana123@gmail.com>
2026-03-11 20:05:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,10 +2,10 @@ class Solution {
     public int rob(int[] nums) {
         int max1=0;
         int max2=0;
-        for(int i=0;i<nums.length;i++){
+        for(int num:nums){
             int t=max1;
             max1=Math.max(max1,max2);
-            max2=t+nums[i];
+            max2=t+num;
         }
         return max1>max2?max1:max2;
     }
