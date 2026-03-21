0ec0aa72ae433dccd230bef2efefab75f412f300
Lakshana PM <pmlakshana123@gmail.com>
2026-03-21 16:18:25 +0530

    Time: 52 ms (75.91%), Space: 119.2 MB (9.76%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] canSeePersonsCount(int[] heights) {
         int n=heights.length;
-        int[] res=new int[n];
         Stack<Integer> stack=new Stack<>();
         for(int i=n-1;i>=0;i--){
             int count=0;
@@ -12,10 +11,9 @@ class Solution {
             if(!stack.isEmpty()){
                 count++;
             }
-            res[i]=count;
             stack.push(heights[i]);
-
+            heights[i]=count;
         }
-        return res;
+        return heights;
     }
 }
