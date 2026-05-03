23630003fc28ed2f36112c34686f8fb7ac318c5e
Lakshana PM <pmlakshana123@gmail.com>
2026-05-03 20:31:15 +0530

    Time: 3 ms (72%), Space: 43.1 MB (70.42%) - LeetHub

---
+++ 0831-masking-personal-information/0831-masking-personal-information.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String emailMasking(String str){
+        str = str.toLowerCase();
+        String res="";
+        res += str.charAt(0);
+        res+=("*").repeat(5);
+        res+=str.substring(str.indexOf("@")-1);
+        return res;
+    }
+    public String numberMasking(String str){
+        String digits="";
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch>='0'&&ch<='9'){
+                digits+=ch;
+            }
+        }
+        int n=digits.length();
+        String local="***-***-"+digits.substring(n-4);
+        if(n==10) return local;
+        if(n==11) return "+*-"+local;
+        if(n==12) return "+**-"+local;
+        return "+***-"+local;
+    }
+    public String maskPII(String s) {
+        if(s.indexOf("@")!=-1){
+            return emailMasking(s);
+        }
+        return numberMasking(s);
+    }
+}
