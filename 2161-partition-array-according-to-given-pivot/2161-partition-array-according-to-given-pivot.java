1a929e633ed709b3b1ad2ccc411890aa139846be
Lakshana PM <pmlakshana123@gmail.com>
2026-06-08 15:26:37 +0530

    Time: 4 ms (99.24%), Space: 159.7 MB (79.84%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,35 +1,22 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> lessThan = new ArrayList<>();
-        ArrayList<Integer> greaterThan = new ArrayList<>();
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<pivot){
-                lessThan.add(nums[i]);
-            }
-            else if(nums[i]>pivot){
-                greaterThan.add(nums[i]);
-            }
-            else{
-                count++;
+        int x=0;
+        int[] result=new int[nums.length];
+        for(int num: nums){
+            if(num<pivot){
+                result[x++]=num;
             }
         }
-        int i=0;
-        while(i<lessThan.size()){
-            nums[i] = lessThan.get(i);
-            i++;
+        for(int num: nums){
+            if(num==pivot){
+                result[x++]=pivot;
+            }
         }
-        while(count>0){
-            nums[i] = pivot;
-            count--;
-            i++;
+        for(int num: nums){
+            if(num>pivot){
+                result[x++]=num;
+            }
         }
-        int j=0;
-        while(j<greaterThan.size()){
-            nums[i] = greaterThan.get(j);
-            j++;
-            i++;
-        }
-        return nums;
+        return result;
     }
 }
