8cc940b484b4739dca7503fad75244a9513473b8
Lakshana PM <pmlakshana123@gmail.com>
2026-06-08 17:19:11 +0530

    Time: 20 ms (98.31%), Space: 48.2 MB (57.63%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -10,9 +10,7 @@ class Solution {
             if(freq[i]==0) continue;
             char ch=(char)(i+'a');
             int res=freq[i]/2;
-            for(int j=0;j<res;j++){
-                sb.append(ch);
-            }
+            sb.append(String.valueOf(ch).repeat(res));
             if(freq[i] % 2 == 1){
                 middle = String.valueOf(ch);
             }
