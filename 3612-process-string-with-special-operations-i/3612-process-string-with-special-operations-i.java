4b6d217553a13c3d3df25873cd1b962cc12920a4
Lakshana PM <pmlakshana123@gmail.com>
2026-06-17 10:30:48 +0530

    Time: 4 ms (76.3%), Space: 55.7 MB (20.74%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String processStr(String s) {
+        if(s.length()==0){
+            return s;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='a'&&ch<='z'){
+                sb.append(ch);
+            }
+            if(ch=='#'){
+                sb.append(sb.toString());
+            }
+            if(ch=='%'){
+                sb.reverse();
+            }
+            if(ch=='*'&&sb.length()>0){
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+
+        return sb.toString();
+    }
+}
